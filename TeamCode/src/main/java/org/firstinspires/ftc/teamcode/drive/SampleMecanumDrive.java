9007c82cb5dd1a4a963fd870ac89bd30273a29ec
AlessioToniolo <Thegooglemail76!>
2021-03-04 16:54:09 -0500

    Sample Max Velocity Bug Fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleMecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleMecanumDrive.java
@@ -29,11 +29,15 @@ import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.hardware.lynx.LynxModule;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 import com.qualcomm.robotcore.hardware.VoltageSensor;
 import com.qualcomm.robotcore.hardware.configuration.typecontainers.MotorConfigurationType;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.teamcode.util.AxesSigns;
+import org.firstinspires.ftc.teamcode.util.BNO055IMUUtil;
 import org.firstinspires.ftc.teamcode.util.DashboardUtil;
 import org.firstinspires.ftc.teamcode.util.LynxModuleUtil;
 
@@ -138,12 +142,12 @@ public class SampleMecanumDrive extends MecanumDrive {
 
         // TODO: if your hub is mounted vertically, remap the IMU axes so that the z-axis points
         // upward (normal to the floor) using a command like the following:
-        // BNO055IMUUtil.remapAxes(imu, AxesOrder.XYZ, AxesSigns.NPN);
+        BNO055IMUUtil.remapAxes(imu, AxesOrder.XYZ, AxesSigns.NPN);
 
-        leftFront = hardwareMap.get(DcMotorEx.class, "leftFront");
-        leftRear = hardwareMap.get(DcMotorEx.class, "leftRear");
-        rightRear = hardwareMap.get(DcMotorEx.class, "rightRear");
-        rightFront = hardwareMap.get(DcMotorEx.class, "rightFront");
+        leftFront = hardwareMap.get(DcMotorEx.class, "leftfront");
+        leftRear = hardwareMap.get(DcMotorEx.class, "leftrear");
+        rightRear = hardwareMap.get(DcMotorEx.class, "rightrear");
+        rightFront = hardwareMap.get(DcMotorEx.class, "rightfront");
 
         motors = Arrays.asList(leftFront, leftRear, rightRear, rightFront);
 
@@ -164,6 +168,10 @@ public class SampleMecanumDrive extends MecanumDrive {
         }
 
         // TODO: reverse any motors using DcMotor.setDirection()
+        leftFront.setDirection(DcMotor.Direction.FORWARD);
+        leftRear.setDirection(DcMotor.Direction.FORWARD);
+        rightFront.setDirection(DcMotor.Direction.REVERSE);
+        rightRear.setDirection(DcMotor.Direction.REVERSE);
 
         // TODO: if desired, use setLocalizer() to change the localization method
         // for instance, setLocalizer(new ThreeTrackingWheelLocalizer(...));
@@ -415,6 +423,6 @@ public class SampleMecanumDrive extends MecanumDrive {
         // Rotate about the z axis is the default assuming your REV Hub/Control Hub is laying
         // flat on a surface
 
-        return (double) imu.getAngularVelocity().zRotationRate;
+        return (double) imu.getAngularVelocity().xRotationRate;
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sample/SampleMaxVelocityTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sample/SampleMaxVelocityTest.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.sample;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 import org.firstinspires.ftc.teamcode.utility.MaxStaticVelocity;
@@ -17,13 +18,16 @@ public class SampleMaxVelocityTest extends LinearOpMode {
     
     @Override
     public void runOpMode() throws InterruptedException {
-        flywheelMotor = hardwareMap.get(DcMotorEx.class, "flywheelmotor");
+        flywheelMotor = hardwareMap.get(DcMotorEx.class, "shooter");
+        flywheelMotor.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
 
         telemetry.addData("Important: ", "Make sure to have a full battery!");
+        telemetry.update();
 
         waitForStart();
 
         while (opModeIsActive()) {
+            flywheelMotor.setPower(1.0);
             currentVelocity = flywheelMotor.getVelocity();
 
             if (currentVelocity > maxVelocity) {
@@ -31,6 +35,7 @@ public class SampleMaxVelocityTest extends LinearOpMode {
             }
 
             telemetry.addData("maximum velocity", maxVelocity);
+            telemetry.addData("static variable velocity", MaxStaticVelocity.maxStaticVelocity);
             telemetry.update();
 
             MaxStaticVelocity.maxStaticVelocity = maxVelocity;
